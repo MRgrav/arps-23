fa7535c782a8346a72ee1fe71dd146a25b969688
uday <udaysubba2004@gmail.com>
2025-08-16 01:07:39 +0530

    fix: card responsiveness

--- resources/js/components/Home/MessageCard.vue
+++ resources/js/components/Home/MessageCard.vue
@@ -19,9 +19,9 @@ const handleImageError = (event: Event) => {
     <div class="px-8 py-16">
         <div class="max-w-7xl mx-auto">
             <h2 class="text-center py-10 text-[#4e71ff]">Leadership Messages</h2>
-            <div class="grid grid-cols-3 gap-10 py-10">
-                <Link v-for="profile in props.profiles" :href="`/profiles/${profile.id}`"
-                    class="bg-[var(--primary-brand-500)]/80 hover:scale-101 transition duration-400 ease-in-out rounded-xs block shadow-md h-full">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 py-10">
+                <Link v-for="profile in props.profiles" :href="`/profiles/${profile.id}`" :key="profile.id"
+                    class="bg-[var(--primary-brand-500)]/80 hover:scale-101 w-full max-w-[400px]  mx-auto transition duration-400 ease-in-out rounded-xs block shadow-md h-full">
                     <div class="-translate-1 hover:-translate-1.5 bg-white w-full h-full border-[var(--primary-brand-500)]/80 border hover:scale-99 transition duration-400 ease-in-out">
                         <div class="aspect-[4/3] object-center object-cover overflow-hidden">
                             <img v-if="profile.image" :src="`/storage/uploads/${profile.image}`"
